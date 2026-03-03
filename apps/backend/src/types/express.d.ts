43b3073e821d6008c5646a273a94b218d78d57da
Rajkumar <thecodeheist01@gmail.com>
2026-03-04 00:02:30 +0530

    site_engineer_routes_done

---
+++ apps/backend/src/types/express.d.ts
@@ -0,0 +1,18 @@
+import { MaterialRequest } from "@repo/db";
+
+declare global {
+    namespace Express {
+        interface User {
+            id: string;
+            role: string;
+            blockId: string | null;
+            siteId: string | null
+        }
+
+        interface Request {
+            user: User;
+        }
+    }
+}
+
+export {}
