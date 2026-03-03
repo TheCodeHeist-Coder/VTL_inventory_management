ed5f5013cfb381e075105129ba728f5b7b581e81
Rajkumar <thecodeheist01@gmail.com>
2026-03-03 18:27:45 +0530

    db_prisma_error_fixed

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -13,8 +13,7 @@ const app: Express = express();
 app.use(cors())
 app.use(express.json());
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-console.log("TYPE:", typeof process.env.DATABASE_URL);
+
 // admin route
 app.use("/api/v1/admin", adminRoute)
 
